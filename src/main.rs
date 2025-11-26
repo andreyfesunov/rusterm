6752de9b41421cfee04473de3433888db60ef246
andreyfesunov <fsnv04@gmail.com>
2025-11-26 10:15:02 +0300

    feat(cli): add daemon option for cli

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use clap::Parser;
+use uuid::Uuid;
 
 mod cli;
 
 fn main() {
-    let cli = cli::Args::parse();
+    let cli = cli::Cli::parse();
+    let instance_id = Uuid::new_v4();
 
     println!("{:?}", cli);
+    print!("Instance ID: {}", instance_id);
 }
